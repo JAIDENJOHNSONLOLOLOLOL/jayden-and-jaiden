17b5739831199c48a392d60f128842de58702177
JAIDENJOHNSONLOLOLOLOL <jayzplayz@outlook.com>
2025-05-22 14:59:36 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,67 @@
+namespace SpriteKind {
+    export const coin = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (Tuxedo_cat.vy == 0) {
+        Tuxedo_cat.vy = -150
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.coin, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    sprites.destroy(otherSprite)
+})
+let Coin: Sprite = null
+let Tuxedo_cat: Sprite = null
+scene.setBackgroundColor(9)
+Tuxedo_cat = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . f . . . 
+    . . . . . . . . . . . . f f f . 
+    . . . . . . . . . . . . f f 9 f 
+    f f f f f f f f f f f f f f f f 
+    . . . . f f f f f f f f f 1 . . 
+    . . . . f f f f f f f f f . . . 
+    . . . . f . f . . . f . f . . . 
+    . . . . 1 . 1 . . . 1 . 1 . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(Tuxedo_cat, 100, 0)
+tiles.setCurrentTilemap(tilemap`level1`)
+Tuxedo_cat.ay = 350
+scene.cameraFollowSprite(Tuxedo_cat)
+for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
+    Coin = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 5 4 4 4 4 5 5 5 f . . . 
+        . f 5 5 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . f 5 4 5 5 5 5 5 5 5 5 5 f . . 
+        . . f 5 5 4 4 5 5 5 5 5 f . . . 
+        . . . f 5 5 5 5 5 5 5 f . . . . 
+        . . . . f f f f f f f . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.coin)
+    tiles.placeOnTile(Coin, value)
+    tiles.setTileAt(value, assets.tile`transparency16`)
+}
